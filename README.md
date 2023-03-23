a6696c834eccedccba0883a9c6a03168ea482296
thierry <thierrypoupon@hotmail.com>
2023-03-23 15:11:58 +0100

    :tada: init

---
+++ README.md
@@ -0,0 +1,15 @@
+# Template React TypeScript buitl with Vite
+
+## Inside
+
+- React
+- Vite
+- TypeScript
+- ESLint
+- Prettier
+
+## Install dependencies
+
+````shell script
+pnpm install
+````
---
+++ src/App.tsx
@@ -0,0 +1,71 @@
+import React, {useState} from 'react'
+import viteLogo from '/vite.svg'
+import reactLogo from './assets/react.svg'
+import typescriptLogo from './assets/typescript.svg'
+import eslintLogo from './assets/eslint-icon.svg'
+import prettierLogo from './assets/prettier-1.svg'
+import pnpmLogo from './assets/pnpm.svg'
+import './App.css'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <div className="App">
+            <div>
+                <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+                    <img
+                        src={reactLogo}
+                        className="logo react"
+                        alt="React logo"
+                    />
+                </a>
+                <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+                    <img
+                        src={typescriptLogo}
+                        className="logo"
+                        alt="typescript logo"
+                    />
+                </a>
+                <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+                    <img
+                        src={eslintLogo}
+                        className="logo"
+                        alt="esLint logo"
+                    />
+                </a>
+                <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+                    <img
+                        src={prettierLogo}
+                        className="logo"
+                        alt="prettier logo"
+                    />
+                </a>
+                <a href="https://reactjs.org" target="_blank" rel="noreferrer">
+                    <img
+                        src={pnpmLogo}
+                        className="logo"
+                        alt="pnpm logo"
+                    />
+                </a>
+            </div>
+            <h1>Vite + React + TypeScript + ESLint + Prettier + pnpm</h1>
+            <div className="card">
+                <button onClick={() => setCount(count => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.tsx</code> and save to test HMR
+                </p>
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+        </div>
+    )
+}
+
+export default App
